ccafb33218ecfd788c7a2f25991659247a95df4c
Zakhar Shyshkin <zahar.shishkin.2001@mail.ru>
2020-09-26 17:29:28 +0300

    Test commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int a = in.nextInt();
         int h, min, sec;
         //z = ((b + Math.sqrt(b*b + 4*a*c))/2 - Math.pow(a, 3) + Math.pow(b, -2));
-        h = a / 3600;
+        h = a / 3600 ;
         min = (a % 3600) / 60;
         sec = (a % 3600) % 60;
         System.out.println(h + " hours " + min + " min " + sec + " sec.");
